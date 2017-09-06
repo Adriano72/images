97ddd91067d9d9356057dab9b1432541aa01b958
Adriano Massi <adrianomassi@gmail.com>
2017-09-07 01:46:48 +0200

    Lezione 18

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,12 +1,23 @@
 // Create our list component
 // Import React
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'https://dummyimage.com/600x400' },
+  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400' },
+  { title: 'Mug', link: 'https://dummyimage.com/600x400' }
+];
 
 // Create our component
 const ImageList = () => {
+  const RenderImages = IMAGES.map(image =>
+    <ImageDetail key={image.title} image={image} />
+  )
+
   return (
-    <ul>
-      <li>Image Detail</li>
+    <ul className="media-list list-group">
+      {RenderImages }
     </ul>
   );
 };
